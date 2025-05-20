3cbc6ee4702b67d311650b96bf86f0f2f928bd4c
Šimon Juda Hloska <hloska@spsejecna.cz>
2025-05-20 20:26:29 +0200

    created event obj and event storing system with json

---
+++ CalendarForChessFans/EventStoring.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection.Metadata;
+using System.Runtime.CompilerServices;
+using System.Xml;
+using Newtonsoft.Json;
+
+namespace CalendarForChessFans
+{
+    public class EventStoring
+    {
+        private const string filepath = "filepath mega";
+        public void SaveEvents(List<Event> events)
+        {
+            var json = JsonConvert.SerializeObject(events, Newtonsoft.Json.Formatting.Indented);
+            File.WriteAllText(filepath, json);
+        }
+
+        public List<Event> LoadEvents()
+        {
+            if (!File.Exists(filepath)) return new List<Event>();
+            var json = File.ReadAllText(filepath);
+            return JsonConvert.DeserializeObject<List<Event>>(json); //might be null, handle
+        }
+    }
+}
---
+++ CalendarForChessFans/WeatherInfo.cs
@@ -0,0 +1,17 @@
+﻿
+namespace CalendarForChessFans
+{
+    public class WeatherInfo
+    {
+        private readonly string apiKey = "key";
+        private readonly HttpClient httpClient = new HttpClient();
+
+        /*public async Task<WeatherInfo> GetWeatherAsync(string location)
+        {
+            var response = await httpClient.GetStringAsync(
+                $"https://api.openweathermap.org/data/2.5/weather?q={location}&appid={apiKey}&units=metric");
+            return JsonConvert.DeserializeObject<WeatherInfo>(response);
+        }*/
+
+    }
+}
